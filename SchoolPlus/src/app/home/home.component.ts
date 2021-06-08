e1cfa021a5fd83c64198a34aa35ca0d43144528d
Daniel Moucha <moudab17@htl-kaindorf.at>
2021-06-08 23:06:17 +0200

    timetable update

--- SchoolPlus/src/app/home/home.component.ts
+++ SchoolPlus/src/app/home/home.component.ts
@@ -2,18 +2,45 @@ import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
+export interface StundenPlan {
+  stunde: number;
+  zeit: string;
+  fach: string;
+  lehrkraft: string;
+  raum: string;
+}
+
+const ELEMENT_DATA: StundenPlan[] = [
+  {stunde: 1, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 2, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 3, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 4, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 5, zeit: '11:35 - 12:25', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 6, zeit: '12:25 - 13:15', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 7, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 8, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 9, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 10, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 11, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {stunde: 12, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  displayedColumns = ['stunde', 'zeit', 'fach', 'lehrkraft', 'raum'];
+  dataSource = ELEMENT_DATA;
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Alle Aufgaben', cols: 5, rows: 1 },
+          { title: 'Alle Aufgaben', cols: 5, rows: 1},
           { title: 'Heutiger Stundenplan', cols: 5, rows: 1 },
           { title: 'Abgaben', cols: 5, rows: 1 },
           { title: 'Prüfungen', cols: 5, rows: 1 }
--- SchoolPlus/src/app/timetable/timetable.component.ts
+++ SchoolPlus/src/app/timetable/timetable.component.ts
@@ -1,34 +1,43 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TimetableService} from "../services/timetable.service";
+import {Timetable} from "../beans/timetable";
 
 @Component({
   selector: 'app-timetable',
   templateUrl: './timetable.component.html',
   styleUrls: ['./timetable.component.css']
 })
-export class TimetableComponent {
-  displayedColumns = ['stunde', 'zeit', 'fach', 'lehrkraft', 'raum'];
-  dataSource = ELEMENT_DATA;
+export class TimetableComponent implements OnInit{
+  displayedColumns = ['hour', 'time', 'subject', 'room', 'teacher'];
+  dataSource :any;
+
+  timetable: Timetable[];
+
+  constructor(private timetableService: TimetableService) {
+    this.timetable = [];
+  }
+
+  ngOnInit() {
+    this.timetableService.findAll().subscribe(value => {
+      this.timetable = value;
+      console.log(this.timetable);
+      this.dataSource = this.timetable;
+    })
+  }
 }
 
-export interface StundenPlan {
-  stunde: number;
-  zeit: string;
-  fach: string;
-  lehrkraft: string;
-  raum: string;
-}
-
+/*
 const ELEMENT_DATA: StundenPlan[] = [
-  {stunde: 1, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 2, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 3, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 4, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 5, zeit: '11:35 - 12:25', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 6, zeit: '12:25 - 13:15', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 7, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 8, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 9, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 10, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 11, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 12, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-];
+  {zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '11:35 - 12:25', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '12:25 - 13:15', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+  {zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
+];*/
