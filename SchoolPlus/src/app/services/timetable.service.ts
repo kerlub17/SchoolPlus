b85b1fe46d607b7b825afe5acf72686b472d5ff1
Daniel Moucha <moudab17@htl-kaindorf.at>
2021-06-08 23:09:48 +0200

    service-class for timetable

---
+++ SchoolPlus/src/app/services/timetable.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Timetable} from "../beans/timetable";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimetableService {
+  private timetableUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.timetableUrl = 'http://localhost:8080/timetable';
+  }
+
+  public findAll(): Observable<Timetable[]> {
+    return this.http.get<Timetable[]>(this.timetableUrl);
+  }
+
+  public save(timetable: Timetable) {
+    return this.http.post<Timetable>(this.timetableUrl, timetable);
+  }
+}
