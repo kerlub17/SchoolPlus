c9ddbd08ad4e854a9a8257fb05d5eef8c4bdbb99
kerlub17 <kerlub17@htl-kaindorf.at>
2021-06-09 14:41:21 +0200

    First backend commit

--- src/main/java/at/kaindorf/schoolplus_backend/SchoolPlusBackendApplication.java
+++ src/main/java/at/kaindorf/schoolplus_backend/SchoolPlusBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 	SchoolPlusBackendApplication {
+public class SchoolPlusBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolPlusBackendApplication.class, args);
