1db75b170b349c790c9f5ac0ef243477994b351d
Daniel Moucha <moudab17@htl-kaindorf.at>
2021-06-27 17:19:16 +0200

    gui improvements

--- SchoolPlus/src/app/home/home.component.ts
+++ SchoolPlus/src/app/home/home.component.ts
@@ -1,60 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
-
-export interface StundenPlan {
-  stunde: number;
-  zeit: string;
-  fach: string;
-  lehrkraft: string;
-  raum: string;
-}
-
-const ELEMENT_DATA: StundenPlan[] = [
-  {stunde: 1, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 2, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 3, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 4, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 5, zeit: '11:35 - 12:25', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 6, zeit: '12:25 - 13:15', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 7, zeit: '08:00 - 08:50', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 8, zeit: '08:50 - 09:40', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 9, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 10, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 11, zeit: '09:40 - 10:30', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-  {stunde: 12, zeit: '10:45 - 11:35', fach: 'AM', lehrkraft: 'BO', raum: 'EG27'},
-];
+import {TimetableService} from "../services/timetable.service";
+import {Timetable} from "../beans/timetable";
+import {GlobalVariables} from "../../global-variables";
+import {NavComponent} from "../nav/nav.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  displayedColumns = ['stunde', 'zeit', 'fach', 'lehrkraft', 'raum'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns = ['hour', 'time', 'subject', 'room', 'teacher', 'activityType'];
+  timetable: Timetable[];
 
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Alle Aufgaben', cols: 5, rows: 1},
-          { title: 'Heutiger Stundenplan', cols: 5, rows: 1 },
-          { title: 'Abgaben', cols: 5, rows: 1 },
-          { title: 'Prüfungen', cols: 5, rows: 1 }
-        ];
-      }
+  constructor(private timetableService: TimetableService) {
+    this.timetable = [];
+  }
 
-      return [
-        { title: 'Alle Aufgaben', cols: 3, rows: 1 },
-        { title: 'Abgaben', cols: 2, rows: 1 },
-        { title: 'Heutiger Stundenplan', cols: 3, rows: 1 },
-        { title: 'Prüfungen', cols: 2, rows: 1 }
-      ];
+  ngOnInit() {
+    this.timetableService.findAll().subscribe(value => {
+      this.timetable = value;
+      console.log(this.timetable);
     })
-  );
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+
 }
